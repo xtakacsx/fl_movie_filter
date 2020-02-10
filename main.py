a8732ed79660436f65816ed94f25881e2fafb562
Paul-Andrei Takács <andreitakacs@gmail.com>
2020-02-11 00:14:22 +0200

    added os variables added QBT

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+import os
 from typing import List
 
+
 from httpx import get
+from qbittorrentapi import LoginFailed, Client
 
 
 class FileList:
     FL_URL = "https://filelist.ro/api.php"
     OMDBAPI_URL = "http://www.omdbapi.com/?apikey="
+    QBT_CLIENT = Client(host="localhost:8080", username="admin", password="adminadmin")
 
-    def __init__(self, username: str, passkey: str, category: List[int], rating: int, omdbapik: str):
+    def __init__(
+        self,
+        username: str,
+        passkey: str,
+        category: List[int],
+        rating: int,
+        omdbapik: str,
+    ):
         self.username = username
         self.passkey = passkey
         self.category = category
@@ -27,6 +38,13 @@ class FileList:
         all_movies = r.json()
         return self._filter_movies(all_movies)
 
+    @property
+    def local_qbt(self) -> list:
+        try:
+            return [torrent.name for torrent in self.QBT_CLIENT.torrents_info()]
+        except LoginFailed:
+            raise LoginFailed
+
     def _filter_movies(self, fl_movies: List[dict]) -> dict:
         checked_id = []
         for movie in fl_movies:
@@ -34,23 +52,29 @@ class FileList:
             if imdb_id not in checked_id:
                 r = get(self.omdbapi, params={"i": imdb_id})
                 movie_rating = float(r.json().get("imdbRating"))
-                movie_plot = r.json().get("Plot")
+                movie["plot"] = r.json().get("Plot")
                 if movie_rating >= self.rating:
                     checked_id.append(imdb_id)
-                    print(f"{movie['name']} RATING: {movie_rating}")
-                    print(f"{movie_plot}")
-                    print(f"{movie['download_link']}\n")
                     yield movie
+
+    def download_movies(self, freelech=None) -> None:
+        # TODO implement freelech param
+        for movie in self.movies:
+            if movie.get("name") not in self.local_qbt:
+                print(f"{movie['name']}")
+                print(f"{movie['plot']}\n")
+                self.QBT_CLIENT.torrents_add(urls=movie.get("download_link"))
+
+        print("DONE")
 
 
 if __name__ == "__main__":
     filelist = FileList(
-        username="filelist user",
-        passkey="filelist apikey",
+        username=os.environ.get("FL_USER"),
+        passkey=os.environ.get("FL_PASS"),
         category=[4, 19],
         rating=8,
-        omdbapik="http://www.omdbapi.com/ key"
+        omdbapik=os.environ.get("OMDBAPIK"),
     )
 
-    gen = filelist.movies
-    print(list(gen))
+    filelist.download_movies()
